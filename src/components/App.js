647f4bdc15e9ad564c0cfa1d00919d9f39a13861
Arnold Kisuri <arnoldkisuri7@gmail.com>
2024-04-22 10:58:21 +0300

    Final edit

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import TopMenu from "./TopMenu";
+import Logo from "./Logo";
+import MainContent from "./MainContent";
+import MenuLinks from "./MenuLinks";
+import Search from "./Search";
+import SideMenu from "./SideMenu";
+import AlbumCard from "./AlbumCard";
+import AlbumList from "./AlbumList";
+import GenrePicker from "./GenrePicker";
 
 function App() {
-  return <div className="app-container">{/* What components go here? */}</div>;
+  return (
+    <div className="app-container">
+      <TopMenu/>
+      <SideMenu/>
+      <MainContent/>
+    </div>
+  );
 }
 
 export default App;
